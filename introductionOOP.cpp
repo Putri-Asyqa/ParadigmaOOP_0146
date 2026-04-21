95dba53c9fcd6ee3be59121cfe1586b6da075fd0
Putri Asyqa <putriasyqa25lindra@gmail.com>
2026-04-21 18:00:32 +0700

    menambahkan prosedur print data

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -6,4 +6,11 @@ public :
     int nim;
     string nama;
     float nilai;
+
+   void printData(){
+        cout << "Nim : "<< nim << endl;   
+        cout << "Nama : " << nama << endl;  
+        cout << "Nilai : " << nilai << endl;
+    }
+
 }
